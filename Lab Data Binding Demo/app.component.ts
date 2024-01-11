c3e61d01c050fc237f894ea16b247d045f041aa4
onemiggy <123311817+onemiggy@users.noreply.github.com>
2024-01-11 15:44:54 +0800

    Add files via upload

---
+++ Lab Data Binding Demo/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular_data_binding';
+  clientName = "Miguel Enriquez"; //property
+  appliedCSS = "black"; //style
+  notAppliedCSS = false;
+  myColor = "yellow";
+
+  clickCount =0
+  clickMe(){
+    this.clickCount++;
+  }
+
+  resetClickCount(){
+    this.clickCount=0;
+  }
+}
+
